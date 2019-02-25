d074d29781d0d4e1aca07173d66079686849d13b
lilongsheng0755 <1226364644@qq.com>
2019-02-25 18:08:08 +0800

    heper request

--- Api/service.php
+++ Api/service.php
@@ -6,3 +6,12 @@
  * Description: API入口
  */
 
+//不使用页面缓存
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+
+//设置返回编码
+//header('Content-Type: text/html;charset=utf-8');
+
+//载入核心文件
+require_once '../core.php';
---
+++ Helper/HelperRequest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Helper;
+
+/**
+ * Author: skylong
+ * CreateTime: 2019-2-25 18:06:52
+ * Description: API请求处理
+ */
+class HelperRequest {
+    //put your code here
+}
