46c11c067b5877e49de109ad294d487c07902675
lilongsheng <1226364644@qq.com>
2019-11-04 12:35:40 +0800

    helper

--- Helper/HelperReturn.php
+++ Helper/HelperReturn.php
@@ -13,14 +13,16 @@ class HelperReturn
     /**
      * 返回json数据格式
      *
-     * @param array $data 需要返回的数据
-     * @param int   $code 默认0正常，负数为异常错误，100-999为业务逻辑错误
+     * @param array  $data     需要返回的数据
+     * @param int    $err_code 默认0正常，负数为异常错误，100-999为业务逻辑错误
+     * @param string $err_msg  错误信息
      */
-    public static function jsonData($data = [], $code = 0)
+    public static function jsonData($data = [], $err_code = 0, $err_msg = '')
     {
         header('Content-Type:application/json;charset=utf-8');
-        $ret = ['code' => 0, 'data' => []];
-        $ret['code'] = (int)$code;
+        $ret = ['err_code' => 0, 'err_msg' => '', 'data' => []];
+        $ret['err_code'] = (int)$err_code;
+        $ret['err_msg'] = (string)$err_msg;
         $ret['data'] = $data ? (array)$data : [];
         die(json_encode($ret));
     }
