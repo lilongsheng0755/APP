007592d5f08a9bdd1c8aee26527e509fe5efb703
lilongsheng <1226364644@qq.com>
2020-02-16 21:07:24 +0800

    dologin

--- Apps/AdminCenter/Admin/Controller/LoginController.php
+++ Apps/AdminCenter/Admin/Controller/LoginController.php
@@ -3,6 +3,7 @@
 namespace Apps\AdminCenter\admin\Controller;
 
 use Apps\AdminCenter\Common\Controller\BaseController;
+use Helper\HelperReturn;
 
 /**
  * Author: skylong
@@ -12,13 +13,22 @@ use Apps\AdminCenter\Common\Controller\BaseController;
 class LoginController extends BaseController
 {
     /**
-     * 用户登录
+     * 用户登录页面
      */
     public function login()
     {
         $tpl_var = [];
         $this->assign($tpl_var);
         $this->display();
+    }
+
+    /**
+     * 处理登录行为
+     *
+     * @param array $request_params 请求参数
+     */
+    public function doLogin($request_params=[]){
+        HelperReturn::jsonData($request_params);
     }
 
     /**
